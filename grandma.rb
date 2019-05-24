581302eedf241cb34a8d3f1fe6a46a98e5f9d409
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-24 05:14:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(sentence)
   if sentence == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif sentence.upcase == false 
+  elsif sentence != sentence.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else 
     return "NO, NOT SINCE 1938!"
